b517389aba9d4ba7dcfd7beab20f29a3ea146ee5
Aymerick Sergeant <Aymerick Sergeant@DESKTOP-P8V8TVH>
2022-10-25 21:32:49 -0400

    Ajout d'une classe abstraite Piece

---
+++ src/main/java/jeu/piece/Piece.java
@@ -0,0 +1,101 @@
+package jeu.piece;
+
+import jeu.Echiquier;
+
+public abstract class Piece {
+	private String nom;
+	protected String couleur; // "blanc" ou "noir"
+	protected int x, y;
+	protected Echiquier echiquier; // echiquier sur lequel se trouve la pièce
+
+	public Piece(String nom, String couleur, int x, int y, Echiquier echiquier) {
+		setNom(nom);
+		setCouleur(couleur);
+		setX(x);
+		setY(y);
+		setEchiquier(echiquier);
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getCouleur() {
+		return couleur;
+	}
+
+	public void setCouleur(String couleur) {
+		this.couleur = couleur;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public Echiquier getEchiquier() {
+		return echiquier;
+	}
+
+	public void setEchiquier(Echiquier echiquier) {
+		this.echiquier = echiquier;
+	}
+
+	public boolean surEchiquier(int x, int y) {
+		return echiquier.surEchiquier(x, y);
+	}
+
+	// Indique si le deplacement de la piece est valide
+	public boolean deplacementValide(int x, int y) {
+		if (!surEchiquier(x, y))
+			return false;
+
+		if (!this.echiquier.caseVide(x, y)) {
+			Piece piece = this.echiquier.getCase(x, y);
+			// si piece de même couleur sur la case visée, deplacement invalide
+			if (!this.isCouleurOpposee(piece))
+				return false;
+		}
+		return false;
+	};
+
+	// Deplacement de la piece aux coordonnées (x,y)
+	public void deplacement(int x, int y) {
+		if (deplacementValide(x, y)) {
+			this.echiquier.setCase(this.x, this.y, null);
+			this.x = x;
+			this.y = y;
+			this.echiquier.setCase(this.x, this.y, this);
+		}
+	}
+
+	public boolean isBlanc() {
+		return getCouleur().equals("blanc");
+	}
+
+	public boolean isNoir() {
+		return getCouleur().equals("noir");
+	}
+
+	public boolean isCouleurOpposee(Piece piece) {
+		if ((this.isBlanc() && piece.isNoir()) || (this.isNoir() && piece.isBlanc()))
+			return true;
+		return false;
+	}
+
+}
