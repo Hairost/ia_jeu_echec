c82d8b8629903bbe1a36a3cfa336d453a9c390b1
paulingaillot <paulingaillot@gmail.com>
2022-11-22 17:58:22 -0500

    Main

--- src/main/java/jeu/App.java
+++ src/main/java/jeu/App.java
@@ -18,11 +18,95 @@ public class App {
         echec.printEchiquier();
         echec.getPossibleMoves();
 
-        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        while (true)
+        {
+            String inputString=input.nextLine();
+            if ("uci".equals(inputString))
+            {
+                inputUCI();
+            }
+            else if (inputString.startsWith("setoption")) // pas necessaire
+            {
+                inputSetOption(inputString);
+            }
+            else if ("isready".equals(inputString))
+            {
+                inputIsReady();
+            }
+            else if ("ucinewgame".equals(inputString))
+            {
+                inputUCINewGame();
+            }
+            else if (inputString.startsWith("position"))
+            {
+                inputPosition(inputString);
+            }
+            else if (inputString.startsWith("go"))
+            {
+                inputGo();
+            }
+            else if (inputString.equals("quit"))
+            {
+                inputQuit();
+            }
+            else if ("print".equals(inputString))
+            {
+                inputPrint();
+            }
+        }
+        input.close();
+    }
 
-        String value = sc.nextLine(); 
 
-
-        sc.close();
+    public static void inputUCI() {
+        System.out.println("id name "+ENGINENAME);
+        System.out.println("id author Jonathan");
+        //options go here
+        System.out.println("uciok");
+    }
+    public static void inputSetOption(String inputString) {
+        //set options
+    }
+    public static void inputIsReady() {
+         System.out.println("readyok");
+    }
+    public static void inputUCINewGame() {
+        //add code here
+    }
+    public static void inputPosition(String input) {
+        input=input.substring(9).concat(" ");
+        if (input.contains("startpos ")) {
+            input=input.substring(9);
+            BoardGeneration.importFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+        }
+        else if (input.contains("fen")) {
+            input=input.substring(4);
+            BoardGeneration.importFEN(input);
+        }
+        if (input.contains("moves")) {
+            input=input.substring(input.indexOf("moves")+6);
+            while (input.length()>0)
+            {
+                String moves;
+                if (UserInterface.WhiteToMove) {
+                    moves=Moves.possibleMovesW(UserInterface.WP,UserInterface.WN,UserInterface.WB,UserInterface.WR,UserInterface.WQ,UserInterface.WK,UserInterface.BP,UserInterface.BN,UserInterface.BB,UserInterface.BR,UserInterface.BQ,UserInterface.BK,UserInterface.EP,UserInterface.CWK,UserInterface.CWQ,UserInterface.CBK,UserInterface.CBQ);
+                } else {
+                    moves=Moves.possibleMovesB(UserInterface.WP,UserInterface.WN,UserInterface.WB,UserInterface.WR,UserInterface.WQ,UserInterface.WK,UserInterface.BP,UserInterface.BN,UserInterface.BB,UserInterface.BR,UserInterface.BQ,UserInterface.BK,UserInterface.EP,UserInterface.CWK,UserInterface.CWQ,UserInterface.CBK,UserInterface.CBQ);
+                }
+                algebraToMove(input,moves);
+                input=input.substring(input.indexOf(' ')+1);
+            }
+        }
+    }
+    public static void inputGo() {
+        String move;
+        if (UserInterface.WhiteToMove) {
+            move=Moves.possibleMovesW(UserInterface.WP,UserInterface.WN,UserInterface.WB,UserInterface.WR,UserInterface.WQ,UserInterface.WK,UserInterface.BP,UserInterface.BN,UserInterface.BB,UserInterface.BR,UserInterface.BQ,UserInterface.BK,UserInterface.EP,UserInterface.CWK,UserInterface.CWQ,UserInterface.CBK,UserInterface.CBQ);
+        } else {
+            move=Moves.possibleMovesB(UserInterface.WP,UserInterface.WN,UserInterface.WB,UserInterface.WR,UserInterface.WQ,UserInterface.WK,UserInterface.BP,UserInterface.BN,UserInterface.BB,UserInterface.BR,UserInterface.BQ,UserInterface.BK,UserInterface.EP,UserInterface.CWK,UserInterface.CWQ,UserInterface.CBK,UserInterface.CBQ);
+        }
+        int index=(int)(Math.floor(Math.random()*(move.length()/4))*4);
+        System.out.println("bestmove "+moveToAlgebra(move.substring(index,index+4)));
     }
 }
